553006d5bb120981fe8ccac1558040543f943733
Marvin Curlee <marvin.curle@gmail.com>
2019-03-27 14:07:28 -0500

    added egress rule

--- main.tf
+++ main.tf
@@ -24,6 +24,18 @@ resource "aws_security_group" "instance" {
       to_port     = 22   
       protocol    = "tcp"    
       cidr_blocks = ["0.0.0.0/0"]  }  
+
+     egress {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+    
+  }
+
+
+
+  
  
 }
 
